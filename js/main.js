97755067f2fbb8f4a24272dcbae082755af44a35
ldemars <ldemars@wisc.edu>
2023-04-29 02:20:19 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,15 +2,18 @@ var map;
 var filePath = ["data/tracts_rank.geojson", 
                 "data/subway_lines.geojson", 
                 "data/subway_stations.geojson"];
+
 //function to instantiate the Leaflet map
 function createMap(){
     
     //create the map
     map = L.map('map', {
         center: [40.7, -74.006],
-        zoom: 11
+        zoom: 11,
+        minZoom: 9
+        
     });
-    
+
     //add OSM base tilelayer
     var baseLayer = {"Open Street Map": L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
