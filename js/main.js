b295f0c73d7456c117210aa84cbc1d1edc9f4943
ldemars <ldemars@wisc.edu>
2023-05-01 22:03:44 -0500

    Implemented tract selection
    Tract popups when clicked added. Interactive click styling added.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var filePath = ["data/tracts_rank.geojson",
 function createMap(){
     //create the map
     map = L.map('map', {
-        center: [40.7, -74.006],
+        center: [40.7, -74.006], //Centered on NYC (lat/long)
         zoom: 11,
         minZoom: 9,
         zoomControl: false //initialize without default zoom control to allow for placement options later
@@ -32,33 +32,35 @@ function createMap(){
     var overlays 
     var layerControl = L.control.layers(baseLayer, overlays).addTo(map);
 
-    
-
-    //call getData functions
+    //Initializes info controller. No data added until later when user clicks on feature.
     createInfoControl(map);
-    tractData(filePath[0],layerControl,map);
+
+    //call data functions - adds each to map. Three separate functions required as we need to perform different tasks for each(?).
+    tractData(filePath[0],layerControl,map);//Pass in filePath array value, overlay controller, and map variables.
     lineData(filePath[1],layerControl,map);
     stationData(filePath[2],layerControl,map);
 };
 
-function createInfoControl(map){
-    var info = L.control();
+function createInfoControl(){
+    
+    info = L.control(); //Custom controller is generated as a GLOBAL variable.
 
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-};
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info" for css
+        this.update();
+        return this._div;
+    };
 
-// method that we will use to update the control based on feature properties passed
-info.update = function (props) {
-    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
-        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-        : 'Hover over a feature');
-};
+    // method that is called to update the control based on feature properties passed
+    info.update = function (props) {
+        this._div.innerHTML = 
+            '<h4>Click to select feature</h4>' +  (props ?
+            '<b>Census Tract ID: ' + props.BoroCT2020 + '</b><br />Accessibility Rank: '+props.Sub_RankMi + ' '
+            : '');
+    };
 
-info.addTo(map);
-
+    info.addTo(map); 
+    return info;
 }
 
 function tractData(input,layerControl){
@@ -89,17 +91,56 @@ function tractData(input,layerControl){
         })
 }
 
+//Add event listeners for hover interaction and click  interaction (tracts only currently)
 function onEachFeature(feature, layer) {
+    prevLayerClicked = null; //Global variable declared for later use in highlightFeatureClick.
+    //Two separate mouse interactions included - hovering & clicking.
     layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        //click: zoomToFeature
+        mouseover: highlightFeatureHover,
+        mouseout: resetHighlightHover,
+        click: highlightFeatureClick
+         
     });
 }
 
-function highlightFeature(e) {
-    var layer = e.target;
+//This function colors the polygon when it is clicked, adds the information to the Info Controller, and resets styling for previously clicked polygon.
+function highlightFeatureClick(e) {
+    
+    //Checks if a pre-existing selection exists. If so it resets the styling.
+    if (prevLayerClicked !== null) {
+        // Reset style  
+        prevLayerClicked.setStyle({
+            weight: 2,
+            fillColor: 'blue',
+            fillOpacity: 0.5
+        });
+    }
 
+
+    var layer = e.target;
+    console.log(layer.feature.properties);
+
+    info.update(layer.feature.properties)//Runs info.update function. (info is a global variable, required for this to work)
+
+    layer.setStyle({ //Sets selection color when a polygon is clicked
+        weight: 5,
+        fillOpacity: 0.7,
+        fillColor: 'red'
+    });
+
+    layer.bringToFront();
+    prevLayerClicked = layer; //Stores clicked layer to be checked later. Basically makes this function a loop if you are repeatedly clicking features.
+}
+
+function highlightFeatureHover(e) {
+    var layer = e.target;
+    //console.log(layer.feature.properties);
+    
+    
+    layer.bindPopup(e.target.feature.properties.BoroCT2020,{className: 'mouseoverpopup'}) //Adds hover pop up to layer object - assign class name for css
+    layer.openPopup(); //Opens pop up while hovering over it.
+    
+    //Sets style when mouse is hovering over polygon
     layer.setStyle({
         weight: 5,
         fillOpacity: 0.7
@@ -108,14 +149,19 @@ function highlightFeature(e) {
     layer.bringToFront();
 }
 
-function resetHighlight(e) {
+
+function resetHighlightHover(e) {
     var layer = e.target;
     
-    layer.setStyle({
+    layer.setStyle({ //Resets style to original
         weight: 2,
+        //fillColor: 'blue',
         fillOpacity: 0.5
     });
+    layer.closePopup(); //Closes popup when mouse goes off of polygon
 }
+
+
 
 function lineData(input,layerControl){
     
